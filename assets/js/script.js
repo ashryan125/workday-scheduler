2e4a306ae600fe4507849fafb98446b9a46baf22
work ashryan <20115553+ashryan125@users.noreply.github.com>
2021-05-26 14:39:51 -0400

    remove minute.js, restructured js file, and added noclick function to savebtns when time is past

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,36 @@
 
-// Display today's day and date
-var todayDate = moment().format('dddd, MMM Do YYYY, h:mm a');
+// Display today's day and date - removed moment.js 5/26/21
+// var todayDate = moment().format('dddd, MMM Do YYYY, h:mm a');
+var todayDate = Date;
 $("#currentDay").html(todayDate);
 
 
 $(document).ready(function () {
-    // saveBtn click listener 
-    $(".saveBtn").on("click", function() {
-        // Get nearby values of the description in JQuery
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
 
-        // Save text in local storage
-        localStorage.setItem(time, text);
-    });
-
-    function timeTracker() {
+    function timeCheck() {  // replaced timeTracker with timeCheck for better clarity
         //get current number of hours.
-        var timeNow = moment().hour();
+        // var currentTime = moment().hour(); - remove moment.js 5/26
+        var date = new Date;
+        var currentTime = date.getHours();  // replaced timeNow with currentTime for easier understanding
 
         // loop over time blocks
         $(".time-block").each(function () {
 
-            var blockTime = parseInt($(this).attr("id").split("task")[1]);
+            var timeBlock = parseInt($(this).attr("id").split("task")[1]);
+
 
             // To check the time and add the classes for background colors
-            if (blockTime < timeNow) {
+            if (timeBlock < currentTime) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
                 $(this).addClass("past noclick");
+        
             }
-            else if (blockTime === timeNow) {
+            else if (timeBlock === currentTime) {
                 $(this).removeClass("past noclick");
                 $(this).removeClass("future");
                 $(this).addClass("present");
+    
             }
             else {
                 $(this).removeClass("present");
@@ -42,9 +39,38 @@ $(document).ready(function () {
 
             }
         })
+        // prevent clicking on save button if time passed - added 5/26
+        $(".btn").each(function () {
+
+            var timeBlock = parseInt($(this).attr("id").split("btn")[1]);
+
+            // To check the time and add the classes for background colors
+            if (timeBlock < currentTime) {
+        
+                $(this).addClass("noclick");
+            }
+            else if (timeBlock === currentTime) {
+                $(this).removeClass("noclick");
+    
+            }
+            else {
+                $(this).removeClass("noclick");
+
+            }
+        })
     };
 
-    // Get item from local storage if any
+        // saveBtn click listener 
+        $(".saveBtn").on("click", function() {
+            // Get nearby values of the description in JQuery
+            var text = $(this).siblings(".description").val();
+            var time = $(this).parent().attr("id");
+    
+            // Save text in local storage
+            localStorage.setItem(time, text);
+        });
+
+    // Get tasks from local storage
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
@@ -56,6 +82,6 @@ $(document).ready(function () {
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-    timeTracker();
+    timeCheck();
 });
 
